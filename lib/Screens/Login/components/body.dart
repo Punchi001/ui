f9d15b2fea94d133801d86da5920e9f5656ef06c
Punchi001 <k.njehu@gmail.com>
2022-05-24 11:03:23 +0100

    change3-01

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -8,11 +8,16 @@ import '../../../components/rounded_password_field.dart';
 import '../../Signup/signup_screen.dart';
 import 'background.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
--- lib/Screens/Signup/components/body.dart
+++ lib/Screens/Signup/components/body.dart
@@ -10,9 +10,14 @@ import 'background.dart';
 import 'or_divider.dart';
 import 'social_icon.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({super.key});
 
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
